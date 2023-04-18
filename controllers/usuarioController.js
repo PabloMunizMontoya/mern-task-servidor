ea1ff9f37808af3de8c1c4d83cea6391158212a4
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-04-17 23:18:13 -0500

    cree el controlador para crear un usuario

---
+++ controllers/usuarioController.js
@@ -0,0 +1,4 @@
+// 9 generamos una function para el controlador como estamos trabajando con redux ponemos  el req res
+exports.crearUsuario = (req, res ) =>{
+    console.log('desde crearUsuario')
+}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,11 +1,13 @@
-//7. rutas para crear usuarios
+//7. rutas para crear usuarios, middleware
 
 const express = require('express')
-
 const router = express.Router()
+const usuarioController = require ('../controllers/usuarioController')
 
-// 9.crea un usuario
-router.post('/', () => {
-
-})
+// 9.crea un usuario, recibimos un request de tipo post hacia esta url /api/usuarios, esta url esta configurada en el router en index, todo lo que esta dentro de las llaves es lo que se ejecuta cuando enviamos el request , para que esta parte no quede tan larga vamos a armar controladores para que controlen lo que pasa dentro de este request.
+/* router.post('/', () => {
+    console.log('creando usuario...')
+}) */
+//10. como ya tenemos el controlador que se ocupa de la funcionalidad esto ya no es un arrow function si no que es la ruta y su controlador, como cada controlador tiene varias funcionalidades le indicamos cual es la que queremos.
+router.post('/', usuarioController.crearUsuario) 
 module.exports = router
