6b13072cd9f733a86bcc65d3970f53e3d4c4389d
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-04-18 20:34:39 -0500

    cree el modelo para usuario

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+//11 creamos el schema de usuarios 
+const UsuarioSchema = mongoose.Schema({
+
+    //13aca vamos a agregar los datos que requiere el modelo para el usuario: 
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+
+    },
+    registro: {
+
+    }
+})
+
+//12 exportamos el modelo con un método de mongoose, entonces el primer parámetro le decimos a mongoose que vamos a crear el modelo usuario con el usuarioSchema
+module.exports = mongoose.model('Usuario', UsuarioSchema)
