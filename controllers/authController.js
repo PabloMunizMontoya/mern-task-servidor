f294b041783706030a502d292457fff0532bf6aa
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-05-16 20:15:59 -0500

    controlador para mostrar el usuario autenticado

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-//20. importamos el modelo de usuario
+ //20. importamos el modelo de usuario
 const Usuario = require('../models/User.js')
 
 //21. importamos bcrypt para comparar el password ya hacheado
@@ -11,7 +11,7 @@ const { validationResult } = require('express-validator')
 const jwt = require('jsonwebtoken')
 
 //24 creamos método para autenticar el usuario 
-exports.autenticarUsuario = async (req, res) => {
+ exports.autenticarUsuario = async (req, res) => {
 
     //25 revisamos si hay errores, la function importada desde express validator tiene como param el request, con este param analiza si hay errores y lo genera como un array
     const errores = validationResult(req)
@@ -39,7 +39,7 @@ exports.autenticarUsuario = async (req, res) => {
             return res.status(400).json({msg: 'Password Incorrecto'})
         }
 
-        //27.2 si todo es correcto creamos el jwt 
+         //27.2 si todo es correcto creamos el jwt 
         const payload = {
             usuario : {
                 id: usuario._id
@@ -55,20 +55,22 @@ exports.autenticarUsuario = async (req, res) => {
             (error, token) => {
                 if(error) throw error
                 res.json({ token, msg: 'Usuario logeado correctamente' })
-            })
+            }) 
 
     } catch (error) {
         console.log(error)
     }
-}
+}  
 
 //obtiene que usuario esta autenticado
-exports.usuarioAutenticado = async (req, res) => {
+ exports.usuarioAutenticado = async (req, res) => {
     try {
-        const usuario = await Usuario.findById(req.usuario.id)
+        //el usuario esta siendo guardado en el middleware de auth, este middleware lo usamos en la ruta entonces podemos acceder al usuario y a su info y compararla con la biblioteca Usuario en. el select es la parte donde le decimos que no queremos pasarla a esa respuesta. 
+        // para refrescar estamos comparando el id del usuario que recién crea una cuenta con los id de la librería Usuario, para obtener el id del usuario que recién crea la cuenta usamos el token que previamente guardamos en el localStorage, sacamos el valor de la memoria y se lo asignamos al clienteAxios para enviar el token via headers y poder hacer la petición correctamente.
+        const usuario = await Usuario.findById(req.usuario.id).select('-password')
         res.json({usuario})
     } catch (error) {
         console.log(error)
         res.status(500).json({msg: 'hubo un error'})
     }
-}
+}  
--- routes/auth.js
+++ routes/auth.js
@@ -8,22 +8,23 @@ const authController = require('../controllers/authController')
 const auth =require('../middleware/auth')
 
 //21. aca van todos los controladores, middleware, las validaciones y el método que usamos para llamar
+// recordar que esta ruta esta definida en
 //iniciamos session
-router.post('/', 
+ router.post('/', 
 
     //21.1 agregamos las reglas de validación, para iniciar sección lo único que requerimos es e mail y password
     [
         check('email','Agrega un email valido').isEmail(),
-        check('password', 'El password debe ser mínimo de 8 caracteres').isLength({ min:6})
+        check('password', 'El password debe ser mínimo de 8 caracteres').isLength({ min:8})
         
     ],
     //26.1 agregamos el controlador a la ruta autenticarUsuario viene del controlador (exports.autenticarUsuario)
     authController.autenticarUsuario
 )
-
+ 
 // obtiene el usuario autenticado
 router.get('/',
     auth,
     authController.usuarioAutenticado
-)
+) 
 module.exports = router
